794f24e7edc1d2c8d5481707b9e15ea1a53ba635
isalazar16 <isalazar030@ikaslea.ehu.eus>
2023-12-11 15:26:41 +0100

    a) eta b) probatzea falta da

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer')
+var path = require('path');
+// const upload = multer({ dest: 'uploads/' })
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/uploads'))
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + '.' + file.mimetype.split('/')[1]
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+const filter = function fileFilter(req, file, cb) {
+    if (file.mimetype.split('/')[1] !== 'png') {
+        cb(null, false)
+    } else {
+        cb(null, true)
+    }
+}
+const limits = {
+    fileSize: 2000000
+}
+
+const upload = multer({ storage: storage, fileFilter: filter, limits: limits })
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.redirect('form.html');
 });
 
